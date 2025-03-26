87afbf66d24ef6ef4e9e87919e1c016e63fed636
Allen Hu <assassincreedv@hotmail.com>
2025-03-26 11:36:59 -0700

    speaker-transcription-diarization

---
+++ main.py
@@ -0,0 +1,16 @@
+from funasr import AutoModel
+
+model = AutoModel(
+    model="speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    vad_model="speech_fsmn_vad_zh-cn-16k-common-pytorch",
+    punc_model="punc_ct-transformer_zh-cn-common-vocab272727-pytorch",
+    spk_model="speech_campplus_sv_zh-cn_16k-common",
+)
+
+if __name__ == '__main__':
+    res = model.generate(
+        input=f"/Users/xiaosonghu/Repo/FunASR/examples/industrial_data_pretraining/sense_voice/audio.wav",
+        language="auto",
+    )
+    for sentence in res[0].get('sentence_info'):
+        print('text:' + str(sentence.__getitem__('text')) + " " + 'speaker: ' + str(sentence.__getitem__('spk')) + ' startTime: ' + str(sentence.__getitem__('start')))
